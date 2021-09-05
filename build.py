b7f67c148a1bb57668097db32798782a4f32614a
Steven Su <lakewood999@gmail.com>
2021-09-04 21:47:29 -0400

    news page, finished about, re-organize files and custom build script

---
+++ build.py
@@ -0,0 +1,18 @@
+from staticjinja import Site
+import json
+
+if __name__ == "__main__":
+    # load in news posts - temporarily before moving to non-static server?
+    f = open("news_posts.json")
+    posts = json.loads(f.read())
+    f.close()
+    # news context
+    news_context = {"posts":posts}
+
+    #site = Site.make_site(contexts=[("news.html",news_context)])
+    site = Site.make_site(env_globals={
+        'posts':posts,
+    },searchpath="templates",outpath="output")
+
+    # enable automatic reloading
+    site.render()
--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 # build jinja templates and sass
-staticjinja build --srcpath=templates --outpath=. && sass sass/style.scss:style.css --no-source-map
+#staticjinja build --srcpath=templates --outpath=. && sass sass/style.scss:output/style.css --no-source-map
+python3 build.py && sass sass/style.scss:output/style.css --no-source-map
