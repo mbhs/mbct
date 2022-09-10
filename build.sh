803670a2824196730b86e5f8e434e6da4316f7fa
JoshDaBosh <jwanggt@gmail.com>
2022-09-10 19:06:27 -0400

    22-23 update

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 # build jinja templates and sass
 #staticjinja build --srcpath=templates --outpath=. && sass sass/style.scss:output/style.css --no-source-map
-python3 build.py && sass sass/style.scss:output/style.css --no-source-map
+python3 build.py && sass sass/style.scss:output/style.css --sourcemap=none
